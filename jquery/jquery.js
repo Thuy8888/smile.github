2cb8912d89e47c4844d3680cfc82647e3fd5be25
thuy8888 <thuthuy6195@gmail.com>
2020-04-19 00:37:36 +0700

    first commit

---
+++ jquery/jquery.js
@@ -0,0 +1,93 @@
+$(function() {
+    function closeSearch() {
+        var $form = $('.navbar-collapse form[role="search"].active')
+        $form.find('input').val('');
+        $form.removeClass('active');
+    }
+
+    // Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
+    $(document).on('click', '.navbar-collapse form[role="search"]:not(.active) button[type="submit"]', function(event) {
+        event.preventDefault();
+        var $form = $(this).closest('form'),
+            $input = $form.find('input');
+        $form.addClass('active');
+        $input.focus();
+    });
+    // ONLY FOR DEMO // Please use $('form').submit(function(event)) to track from submission
+    // if your form is ajax remember to call `closeSearch()` to close the search container
+    $(document).on('click', '.navbar-collapse form[role="search"].active button[type="submit"]', function(event) {
+        event.preventDefault();
+        var $form = $(this).closest('form'),
+            $input = $form.find('input');
+        $('#showSearchTerm').text($input.val());
+        closeSearch()
+    });
+});
+$(document).ready(function() {
+    $("#myBtn").click(function() {
+        $("#myModal").modal();
+    });
+});
+
+
+//scroll to top
+$(document).ready(function() {
+
+    // hide #back-top first
+    $("#back-top").hide();
+
+    // fade in #back-top
+    $(function() {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function() {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 1800);
+            return false;
+        });
+    });
+
+});
+$(function() {
+    // Remove Search if user Resets Form or hits Escape!
+    $('body, .navbar_has_search form[role="search"] button[type="reset"]').on('click keyup', function(event) {
+        console.log(event.currentTarget);
+        if (event.which == 27 && $('.navbar_has_search form[role="search"]').hasClass('active') ||
+            $(event.currentTarget).attr('type') == 'reset') {
+            closeSearch();
+        }
+    });
+
+    function closeSearch() {
+        var $form = $('.navbar_has_search form[role="search"].active')
+        $form.find('input').val('');
+        $form.removeClass('active');
+    }
+
+    // Show Search if form is not active // event.preventDefault() is important, this prevents the form from submitting
+    $(document).on('click', '.navbar_has_search form[role="search"]:not(.active) button[type="submit"]', function(event) {
+        event.preventDefault();
+        var $form = $(this).closest('form'),
+            $input = $form.find('input');
+        $form.addClass('active');
+        $input.focus();
+
+    });
+    // ONLY FOR DEMO // Please use $('form').submit(function(event)) to track from submission
+    // if your form is ajax remember to call `closeSearch()` to close the search container
+    $(document).on('click', '.navbar_has_search form[role="search"].active button[type="submit"]', function(event) {
+        event.preventDefault();
+        var $form = $(this).closest('form'),
+            $input = $form.find('input');
+        $('#showSearchTerm').text($input.val());
+        closeSearch()
+    });
+});
